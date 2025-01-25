db8e0f75eb7d6caef8e5c1891693fa380980958e
Asif Sayyed <asifdotexe@gmail.com>
2025-01-25 14:35:47 +0530

    creating a perceptron function

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+This file contains the code for the following:
+
+1. Perceptron gate implementation and simulation of the following gates
+    a. AND
+    b. OR
+    c. NAND
+    d. NOR
+"""
+
+from typing import Callable
+
+def perceptron(weights: list[float], bias: float) -> Callable[[list[int]],int]:
+    """ Create a perceptron function with given weights and bias.
+
+    :param weights: List of weights given for inputs.
+    :type weights: list[float]
+    :param bias: Bias Term
+    :type bias: float
+    :returns: A function that computes the perceptron output for given inputs
+    :rtype: Callable[[list[int],int]
+    """
+    pass
+
+
+
+
+
+
+
